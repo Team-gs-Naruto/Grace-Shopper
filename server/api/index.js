0ffc72b2df337005f5c0f288bdb86eead3dc4585
Charlies Pan <Cp81089@gmail.com>
2020-06-21 13:24:30 -0400

    fix: Merge Conflicts

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 module.exports = router
+const checkFuncs = require('./permissions.middleware')
 
-router.use('/users', require('./users'))
 router.use('/sneakers', require('./sneakers'))
+router.use('/', checkFuncs.notAGuest, checkFuncs.isAdmin, (req, res, next) => {
+  next()
+})
+router.use('/users', require('./users'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
