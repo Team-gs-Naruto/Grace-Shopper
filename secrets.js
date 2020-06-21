4caefb15acd835d1bcf60292282168ec517f8a84
Alex <albangiyev@gmail.com>
2020-06-20 23:43:01 -0400

    feat: added config vars to heroku

---
+++ secrets.js
@@ -0,0 +1,6 @@
+process.env.GOOGLE_CLIENT_ID =
+  '549984685991-5ks5nt1ru5525c890p6pukq0udqs71ln.apps.googleusercontent.com'
+
+process.env.GOOGLE_CLIENT_SECRET = 'p058R0_jlso0XVHe0FxvaTXx'
+
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
