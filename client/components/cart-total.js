ebaae3bf4f50f80ddd77988c6eb07c6b5f083bcc
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-06-23 12:58:33 -0400

    feat: cart reducer

---
+++ client/components/cart-total.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const reduceCart = cart => {
+  let quantities = cart.map(item => item.purchase.quantity)
+  let price = cart.map(item => item.purchase.priceAtPurchase)
+  let total = 0
+  for (let i = 0; i < quantities.length; i++) {
+    total += quantities[i] * price[i]
+  }
+  return total
+}
+
+const CartTotal = props => {
+  const {cartItems, isLoggedIn} = props
+  let total
+
+  if (isLoggedIn) {
+    total = reduceCart(cartItems)
+  }
+  return (
+    <div>
+      <p>Your total is ${total}</p>
+    </div>
+  )
+}
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user.id,
+    cartItems: state.cart
+  }
+}
+export default connect(mapState)(CartTotal)
